954a484b4fc95d41fd707dbdf1238fb8e21e155c
Daniel-C-Igbokwe <107478740+Daniel-C-Igbokwe@users.noreply.github.com>
2025-01-01 21:52:19 -0600

    I'm officially working on building this new balance clone website

---
+++ src/assets/assets.js
@@ -0,0 +1,69 @@
+import clothes_1 from "./clothes_1.jpg";
+import contact_img from "./contact_img";
+import cozy_gifts_1 from "./cozy_gifts_1.webp";
+import cozy_gifts_3 from "./cozy_gifts_3.webp";
+import cozy_gifts_4 from "./cozy_gifts_4.webp";
+import cozy_gifts_5 from "./cozy_gifts_5.webp";
+import gift_for_kids_1 from "./gift_for_kids_1.webp";
+import gift_for_kids_2 from "./gift_for_kids_2.webp";
+import gift_for_kids_3 from "./gift_for_kids_3.webp";
+import gift_for_kids_4 from "./gift_for_kids_4.webp";
+import gift_for_kids_5 from "./gift_for_kids_5.webp";
+import gift_for_kids_6 from "./gift_for_kids_6.webp";
+import gift_for_the_game_1 from "./gift_for_the_game_1.webp";
+import gift_for_the_game_2 from "./gift_for_the_game_2.webp";
+import gift_for_the_game_3 from "./gift_for_the_game_3.webp";
+import gift_for_the_game_4 from "./gift_for_the_game_4.webp";
+import gift_for_the_game_5 from "./gift_for_the_game_5.webp";
+import gift_for_the_game_6 from "./gift_for_the_game_6.webp";
+import gift_img from "./gift_img.jpg";
+import gift_made_in_USA_1 from "./gift_made_in_USA_1.webp";
+import gift_made_in_USA_2 from "./gift_made_in_USA_2.webp";
+import gift_made_in_USA_3 from "./gift_made_in_USA_3.webp";
+import gift_made_in_USA_4 from "./gift_made_in_USA_4.webp";
+import gift_made_in_USA_5 from "./gift_made_in_USA_5.webp";
+import gift_made_in_USA_6 from "./gift_made_in_USA_6.webp";
+import hero_img from "./hero_img.jpg";
+import men_clothes_1 from "./men_clothes_1.webp";
+import men_clothes_2 from "./men_clothes_2.webp";
+import men_clothes_3 from "./men_clothes_3.webp";
+import middle_pic from "./middle_pic.webp";
+import shoe_1 from "./shoe_1.webp";
+import shoe_2 from "./shoe_2.webp";
+import shoe_3 from "./shoe_3.webp";
+import shoe_4 from "./shoe_4.webp";
+import shoe_5 from "./shoe_5.webp";
+import shoe_6 from "./shoe_6.webp";
+import shoe_7 from "./shoe_7.webp";
+import women_1 from "./women_1.webp";
+import women_2 from "./women_2.webp";
+import women_3 from "./women_3.webp";
+
+import menu_icon from "./menu_icon.png";
+import bin_icon from "./bin_icon.png";
+import cart_icon from "./cart_icon.png";
+import cross_icon from "./cross_icon.png";
+import dropdown_icon from "./dropdown_icon.png";
+import exchange_icon from "./exchange_icon.png";
+
+export const assets = [
+  {
+    menu_icon,
+    hero_img,
+    cart_icon,
+    dropdown_icon,
+    exchange_icon,
+    cross_icon,
+    bin_icon,
+  },
+];
+
+export const products = [
+  {
+    _id: "aaaa",
+    name: 327,
+    description: "A cool women's shoe"
+    image: [gift_for_kids_1],
+    size: "Small",
+  },
+];
